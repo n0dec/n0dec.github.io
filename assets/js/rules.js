a80cc76a07096d93f7547a4bc9709be254841407
n0dec <n0dec.sec@gmail.com>
2018-08-12 22:22:02 +0200

    Example graph

--- assets/js/rules.js
+++ assets/js/rules.js
@@ -14,7 +14,7 @@ function format_to_rule(data) {
 
 			for (var i = 1; i < raw_split2.length; i++) {
 				var f_split = raw_split2[i].split(': ')
-				if (f_split[1] == null){
+				if (f_split[1] == null && f_split[0] != ""){
 					f_split[0] = f_split[0].replace(":","");
 					f_split[1] = "";
 				}
@@ -41,7 +41,7 @@ function text_to_rule(data){
 
 			for (var i = 14; i < raw_split2.length; i++) {
 				var f_split = raw_split2[i].split(': ')
-				if (f_split[1] == null){
+				if (f_split[1] == null && f_split[0] != ""){
 					f_split[0] = f_split[0].replace(":","");
 					f_split[1] = "";
 				}
@@ -247,4 +247,163 @@ function get_weight(data, item){
 			result += 1;
 	}
 	return result;
+}
+
+function grаph_example(){
+	var example = `Process Create:
+RuleName:
+UtcTime: 2018-08-09 17:52:27.422
+ProcessGuid: {365ABB72-7F5B-5B6C-0000-0010E4470200}
+ProcessId: 2544
+Image: C:\\Windows\\System32\\cmd.exe
+FileVersion: 6.1.7601.17514 (win7sp1_rtm.101119-1850)
+Description: Windows Command Processor
+Product: Microsoft� Windows� Operating System
+Company: Microsoft Corporation
+CommandLine: C:\\Windows\\system32\\cmd.exe /c ""C:\\Users\\IEUser\\Desktop\\sample.cmd" "
+CurrentDirectory: C:\\Windows\\system32\\
+User: IEWIN7\\IEUser
+LogonGuid: {365ABB72-7F55-5B6C-0000-00203DD80000}
+LogonId: 0xd83d
+TerminalSessionId: 1
+IntegrityLevel: High
+Hashes: MD5=AD7B9C14083B52BC532FBA5948342B98,SHA256=17F746D82695FA9B35493B41859D39D786D32B23A9D2E00F4011DEC7A02402AE
+ParentProcessGuid: {365ABB72-7F5B-5B6C-0000-00108C3E0200}
+ParentProcessId: 2512
+ParentImage: C:\\Windows\\explorer.exe
+ParentCommandLine: C:\\Windows\\explorer.exe /factory,{75dff2b7-6936-4c06-a8bb-676a7b00b24b} -Embedding
+
+Process Create:
+RuleName:
+UtcTime: 2018-08-09 17:52:27.452
+ProcessGuid: {365ABB72-7F5B-5B6C-0000-00103F4A0200}
+ProcessId: 2576
+Image: C:\\Windows\\System32\\PING.EXE
+FileVersion: 6.1.7600.16385 (win7_rtm.090713-1255)
+Description: TCP/IP Ping Command
+Product: Microsoft� Windows� Operating System
+Company: Microsoft Corporation
+CommandLine: ping  8.8.8.8
+CurrentDirectory: C:\\Windows\\system32\\
+User: IEWIN7\\IEUser
+LogonGuid: {365ABB72-7F55-5B6C-0000-00203DD80000}
+LogonId: 0xd83d
+TerminalSessionId: 1
+IntegrityLevel: High
+Hashes: MD5=6242E3D67787CCBF4E06AD2982853144,SHA256=4CA10DBA7FF487FDB3F1362A3681D7D929F5AA1262CDFD31B04C30826983FB1D
+ParentProcessGuid: {365ABB72-7F5B-5B6C-0000-0010E4470200}
+ParentProcessId: 2544
+ParentImage: C:\\Windows\\System32\\cmd.exe
+ParentCommandLine: C:\\Windows\\system32\\cmd.exe /c ""C:\\Users\\IEUser\\Desktop\\sample.cmd" "
+
+Process Create:
+RuleName:
+UtcTime: 2018-08-09 17:52:30.617
+ProcessGuid: {365ABB72-7F5E-5B6C-0000-001098D10200}
+ProcessId: 2896
+Image: C:\\Windows\\System32\\timeout.exe
+FileVersion: 6.1.7600.16385 (win7_rtm.090713-1255)
+Description: timeout - pauses command processing
+Product: Microsoft� Windows� Operating System
+Company: Microsoft Corporation
+CommandLine: timeout  5
+CurrentDirectory: C:\\Windows\\system32\\
+User: IEWIN7\\IEUser
+LogonGuid: {365ABB72-7F55-5B6C-0000-00203DD80000}
+LogonId: 0xd83d
+TerminalSessionId: 1
+IntegrityLevel: High
+Hashes: MD5=419A5EF8D76693048E4D6F79A5C875AE,SHA256=C4EFAFC49D46DBA7E89CD94892799C739A3103F724FEC9BE494BF1267FDB059E
+ParentProcessGuid: {365ABB72-7F5B-5B6C-0000-0010E4470200}
+ParentProcessId: 2544
+ParentImage: C:\\Windows\\System32\\cmd.exe
+ParentCommandLine: C:\\Windows\\system32\\cmd.exe /c ""C:\\Users\\IEUser\\Desktop\\sample.cmd" "
+
+Process Create:
+RuleName:
+UtcTime: 2018-08-09 17:52:35.163
+ProcessGuid: {365ABB72-7F63-5B6C-0000-0010C6330300}
+ProcessId: 3016
+Image: C:\\Windows\\System32\\systeminfo.exe
+FileVersion: 6.1.7600.16385 (win7_rtm.090713-1255)
+Description: Displays system information
+Product: Microsoft� Windows� Operating System
+Company: Microsoft Corporation
+CommandLine: systeminfo
+CurrentDirectory: C:\\Windows\\system32\\
+User: IEWIN7\\IEUser
+LogonGuid: {365ABB72-7F55-5B6C-0000-00203DD80000}
+LogonId: 0xd83d
+TerminalSessionId: 1
+IntegrityLevel: High
+Hashes: MD5=258B2ED54FC7F74E2FDCCE5861549C1A,SHA256=DCE2C5EA1DA23E63EEFF2620141FBC2CA45FC4A64F40AF89E722F4B5FAFED27C
+ParentProcessGuid: {365ABB72-7F5B-5B6C-0000-0010E4470200}
+ParentProcessId: 2544
+ParentImage: C:\\Windows\\System32\\cmd.exe
+ParentCommandLine: C:\\Windows\\system32\\cmd.exe /c ""C:\\Users\\IEUser\\Desktop\\sample.cmd" "
+
+File created:
+RuleName:
+UtcTime: 2018-08-09 17:52:40.321
+ProcessGuid: {365ABB72-7F5B-5B6C-0000-0010E4470200}
+ProcessId: 2544
+Image: C:\\Windows\\system32\\cmd.exe
+TargetFilename: C:\\Users\\IEUser\\AppData\\Local\\Temp\\updates.exe
+CreationUtcTime: 2018-08-09 17:52:40.321
+
+Process Create:
+RuleName:
+UtcTime: 2018-08-09 17:52:40.331
+ProcessGuid: {365ABB72-7F68-5B6C-0000-0010721F0400}
+ProcessId: 3184
+Image: C:\\Windows\\System32\\certutil.exe
+FileVersion: 6.1.7601.18151 (win7sp1_gdr.130512-1533)
+Description: CertUtil.exe
+Product: Microsoft� Windows� Operating System
+Company: Microsoft Corporation
+CommandLine: certutil.exe  -urlcache -split -f "https://download.sysinternals.com/files/PSTools.zip" %LOCALAPPDATA%\\resources.zip
+CurrentDirectory: C:\\Windows\\system32\\
+User: IEWIN7\\IEUser
+LogonGuid: {365ABB72-7F55-5B6C-0000-00203DD80000}
+LogonId: 0xd83d
+TerminalSessionId: 1
+IntegrityLevel: High
+Hashes: MD5=0D52559AEF4AA5EAC82F530617032283,SHA256=48850FB7229D99E48C3A749556684E962587058D612C659C58F8B8DB2D00ABEE
+ParentProcessGuid: {365ABB72-7F5B-5B6C-0000-0010E4470200}
+ParentProcessId: 2544
+ParentImage: C:\\Windows\\System32\\cmd.exe
+ParentCommandLine: C:\\Windows\\system32\\cmd.exe /c ""C:\\Users\\IEUser\\Desktop\\sample.cmd" "
+
+Network connection detected:
+RuleName:
+UtcTime: 2018-08-09 17:52:40.439
+ProcessGuid: {365ABB72-7F68-5B6C-0000-0010721F0400}
+ProcessId: 3184
+Image: C:\\Windows\\System32\\certutil.exe
+User: IEWIN7\\IEUser
+Protocol: tcp
+Initiated: true
+SourceIsIpv6: false
+SourceIp: 10.0.2.15
+SourceHostname: IEWIN7
+SourcePort: 49163
+SourcePortName:
+DestinationIsIpv6: false
+DestinationIp: 152.199.19.160
+DestinationHostname:
+DestinationPort: 443
+DestinationPortName: https
+
+File created:
+RuleName:
+UtcTime: 2018-08-09 17:52:40.784
+ProcessGuid: {365ABB72-7F68-5B6C-0000-0010721F0400}
+ProcessId: 3184
+Image: C:\\Windows\\system32\\certutil.exe
+TargetFilename: C:\\Users\\IEUser\\AppData\\Local\\resources.zip
+CreationUtcTime: 2018-08-09 17:52:40.784
+
+`;
+	document.getElementById("raw").innerHTML = example;
+	to_graph();
 }
